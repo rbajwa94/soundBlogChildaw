5fd3c7b411acebaff39ad4122678792dca9e3fd4
Student <student@CVS-R212D014.nic.bc.ca>
2018-11-26 12:15:43 -0800

    adding concert custom link

--- wp-content/plugins/concerts-custom-post.php
+++ wp-content/plugins/concerts-custom-post.php
@@ -23,3 +23,56 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with {Plugin Name}. If not, see {License URI}.
 */
+function my_custom_posttypes() {
+    
+        $labels = array(
+       'name'               => 'Concerts ',
+       'singular_name'      => 'Concert',
+       'menu_name'          => 'Concerts',
+       'name_admin_bar'     => 'Concerts',
+       'add_new'            => 'Add New',
+       'add_new_item'       => 'Add New Concert',
+       'new_item'           => 'New Concert',
+       'edit_item'          => 'Edit Concert',
+       'view_item'          => 'View Concert',
+       'all_items'          => 'All Concerts',
+       'search_items'       => 'Search Concerts',
+       'parent_item_colon'  => 'Parent Concerts:',
+       'not_found'          => 'No Concerts found.',
+       'not_found_in_trash' => 'No Concerts found in Trash.',
+   );
+
+   $args = array(
+       'labels'             => $labels,
+       'public'             => true,
+       'publicly_queryable' => true,
+       'show_ui'            => true,
+       'show_in_menu'       => true,
+        'menu_icon'          => 'dashicons-format-image',
+        'capability_type'    => 'post',
+       'has_archive'        => true,
+       'hierarchical'       => false,
+       'query_var'          => true,
+       'rewrite'            => array( 'slug' => 'concerts' ),
+       'capability_type'    => 'post',
+       'has_archive'        => true,
+       'hierarchical'       => false,
+       'menu_position'      => 5,
+       'supports'           => array( 'title', 'editor', 'thumbnail' ) 
+
+   );
+   register_post_type( 'concerts', $args);
+}
+add_action ( 'init', 'my_custom_posttypes' );
+function my_rewrite_flush() {
+    // First, we "add" the custom post type via the above written function.
+    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
+    // They are only referenced in the post_type column with a post entry, 
+    // when you add a post of this CPT.
+    my_custom_posttypes();
+
+    // ATTENTION: This is *only* done during plugin activation hook in this example!
+    // You should *NEVER EVER* do this on every page load!!
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'my_rewrite_flush' );
